9d067b37799743c7113ea37172011c66b4e49ed0
Muhammad Masum <48872682+masummuhammad@users.noreply.github.com>
2024-04-24 20:34:17 +0600

    Update proxyReader.py

--- proxyReader.py
+++ proxyReader.py
@@ -1,1 +1,16 @@
 
+from itertools import cycle
+
+# List of proxies
+proxies = [
+    'http://proxy1.example.com:8080',
+    'http://proxy2.example.com:8080',
+    # Add more proxies as needed
+]
+proxy_pool = cycle(proxies)
+
+# Create a cycle iterator for the proxies
+def retriveProxy():
+    proxy=next(proxy_pool)
+    return {"http":proxy,"https":proxy}
+
