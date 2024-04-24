d9b7b80ada360d48201cf513a090dbd0e67b05e5
Muhammad Masum <48872682+masummuhammad@users.noreply.github.com>
2024-04-24 20:43:06 +0600

    Update proxyReader.py

--- proxyReader.py
+++ proxyReader.py
@@ -3,9 +3,7 @@ from itertools import cycle
 
 # List of proxies
 proxies = [
-    'http://proxy1.example.com:8080',
-    'http://proxy2.example.com:8080',
-    # Add more proxies as needed
+    'http://residential.dinamikproxy.com:11250:C05ax7:Rkutyc1_country-tr'
 ]
 proxy_pool = cycle(proxies)
 
